97777233957b5679002d6c07786977202486e0b4
Metallicode <41553615+Metallicode@users.noreply.github.com>
2019-12-06 04:20:47 +0200

    06/12/2019 - dom

---
+++ 06_fft_analysis/python_fft.py
@@ -0,0 +1,15 @@
+import scipy.io.wavfile as wavfile
+import scipy
+import scipy.fftpack as fftpk
+import numpy as np
+from matplotlib import pyplot as plt
+
+s_rate, signal = wavfile.read("tibet.wav") 
+
+FFT = abs(scipy.fft(signal))
+freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+
+plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])                                                          
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.show()
